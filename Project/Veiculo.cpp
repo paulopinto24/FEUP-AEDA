a468a76b4e2ca4ea10e4f77b0681786a2112a0c4
paulopinto24 <47751746+paulopinto24@users.noreply.github.com>
2019-11-07 10:41:04 +0000

    Some changes

--- Project/Veiculo.cpp
+++ Project/Veiculo.cpp
@@ -1,28 +1,37 @@
 #include "Veiculo.h"
 
-Veiculo::Veiculo(string mat, string mar) {
-	matricula = mat;
-	marca = mar;
+Veiculo::Veiculo(string m, string t, string da) {
+	tipo = t;
+	marca = m;
+	dataAquisicao = da;
 }
 
 string Veiculo::getMarca() {
 	return marca;
 }
 
-string Veiculo::getMatricula() {
-	return matricula;
+string Veiculo::getTipo() {
+	return tipo;
+}
+
+string Veiculo::getDataAquisicao() {
+	return dataAquisicao;
 }
 
 void Veiculo::setMarca(string m) {
 	marca = m;
 }
 
-void Veiculo::setMatricula(string m) {
-	matricula = m;
+void Veiculo::setTipo(string t) {
+	tipo = t;
+}
+
+void Veiculo::setDataAquisicao(string ds) {
+	dataAquisicao = ds;
 }
 
 bool Veiculo::operator==(const Veiculo & v1) const {
-	if (this->marca == v1.marca && this->matricula == v1.matricula)
+	if (this->marca == v1.marca && this->tipo == v1.tipo)
 		return true;
 	return false;
 }
