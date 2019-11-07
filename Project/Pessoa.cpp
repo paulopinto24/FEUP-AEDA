879ff949d87c23e43a0c8586f0e6bc955c2a1d02
paulopinto24 <47751746+paulopinto24@users.noreply.github.com>
2019-11-07 10:30:54 +0000

    Added some classes

---
+++ Project/Pessoa.cpp
@@ -0,0 +1,25 @@
+#include "Pessoa.h"
+
+Pessoa::Pessoa(string n, string nif) {
+	nome = n;
+	NIF = nif;
+}
+
+Cliente::Cliente(string n, string nif, string e, string m) :Pessoa(n, nif) {
+	email = e;
+	morada = m;
+}
+
+Funcionario::Funcionario(string n, string nif, string dn, double s) : Pessoa(n, nif) {
+	dataNasc = dn;
+	salario = s;
+}
+
+Administrativo::Administrativo(string n, string nif, string dn, double s, string f) : Funcionario(n, nif, dn, s) {
+	funcao = f;
+}
+
+Entregador::Entregador(string n, string nif, string dn, double s, Veiculo v) : Funcionario(n, nif, dn, s) {
+	veiculo.setMarca(v.getMarca);
+	veiculo.setMatricula(v.getMatricula);
+}
